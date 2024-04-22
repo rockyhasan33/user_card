1b825e9d8871bbca23d21bb45131972d47b497b6
rockybul <rockyhasan403@gmail.com>
2024-04-22 18:00:52 +0530

    adding javascript code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const url = 'https://api.github.com/users';
 
 fetch(url)
 .then(response => response.json())
-.then(id => console.log(id[1]))
+.then(id => console.log(id[0]))
 // https://avatars.githubusercontent.com/u/1?v=4
 
 //  ************* Select Element *************
@@ -12,5 +12,42 @@ const userName = document.querySelector('.name');
 const randomBtn = document.querySelector('#random-btn');
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
+console.log(rightArrow)
 
 let index = 0;
+
+rightArrow.addEventListener('click', () => {
+    index++;
+    if(index > 29) {
+        index = 0;
+    }
+    fetch(url)
+.then(response => response.json())
+.then(id => {
+    userImg.src = id[index].avatar_url;
+    userName.textContent = id[index].login;
+})
+})
+
+leftArrow.addEventListener('click', () => {
+    index--;
+    if(index < 0) {
+        index = 29;
+    }
+    fetch(url)
+.then(response => response.json())
+.then(id => {
+    userImg.src = id[index].avatar_url;
+    userName.textContent = id[index].login;
+})
+})
+
+randomBtn.addEventListener('click', () => {
+    index = Math.floor(Math.random() * 30);
+    fetch(url)
+    .then(response => response.json())
+    .then(id => {
+        userImg.src = id[index].avatar_url;
+        userName.textContent = id[index].login;
+    })
+})
